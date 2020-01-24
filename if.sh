9d706785869c6499e1c7773b91aaa075cff623ee
Jason <jason@prodigyfinance.com>
2020-01-24 16:22:12 +0000

    working

--- if.sh
+++ if.sh
@@ -3,40 +3,41 @@
 ## Get linux version to run specfic scripts against it.
 version=$(cat /etc/issue | cut -d "." -f1)
  
-if [ "$version" = "Linux Mint 19" || "$version" = "Ubuntu 18" ]
+if [ "$version" = "Linux Mint 19" ] || [ "$version" = "Ubuntu 18" ]
+#if [ "$version" = "Linux Mint 19" ]
 then
     	echo "Version is $version"
-	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-	deb http://repo.pritunl.com/stable/apt bionic main
-	EOF
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt bionic main
+EOF
 
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-	sudo apt-get update
-	sudo apt-get install pritunl-client-electron -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
+sudo apt-get install pritunl-client-electron -y
 
-elif [ "$version" = "Linux Mint 18" || "$version" = "Ubuntu 16" ]
+elif [ "$version" = "Linux Mint 18" ] || [ "$version" = "Ubuntu 16" ]
 then
-	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-	deb http://repo.pritunl.com/stable/apt xenial main
-	EOF
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt xenial main
+EOF
 
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-	sudo apt-get update
-	sudo apt-get install pritunl-client-electron
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
+sudo apt-get install pritunl-client-electron
 
 elif [ "$version" = "Ubuntu 19" ]
 then
-	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-	deb http://repo.pritunl.com/stable/apt disco main
-	EOF
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt disco main
+EOF
 
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-	sudo apt-get update
-	sudo apt-get install pritunl-client-electron
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo apt-get update
+sudo apt-get install pritunl-client-electron
 else 
     	echo "$version not supported by this script, only Ubuntu 16, Ubuntu 18, Linux Mint 18 and Linux Mint 19 are supported!!"
-fi
 exit
+fi
 #################
 ####################
 #fi
