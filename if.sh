a0e71a316f2bb49f943f4a72900726bcd6cdaf28
Jason <jason@prodigyfinance.com>
2020-01-24 15:27:42 +0000

    update

--- if.sh
+++ if.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
  
+## Get linux version to run specfic scripts against it.
 version=$(cat /etc/issue | cut -d "." -f1)
-#version=`cat /etc/issue | cut -d "." -f1`
  
-#if [ "$version" = "Linux Mint 19" ]
 if [ "$version" = "Linux Mint 19" || "$version" = "Ubuntu 18" ]
 then
-    echo "Version is $version"
-sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-deb http://repo.pritunl.com/stable/apt bionic main
-EOF
+    	echo "Version is $version"
+	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+	deb http://repo.pritunl.com/stable/apt bionic main
+	EOF
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get update
-sudo apt-get install pritunl-client-electron -y
-#pritunl-client-electron
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	sudo apt-get update
+	sudo apt-get install pritunl-client-electron -y
 
-elif [ "$version" = "Ubuntu 18" || "$version" = "Mint 18" ]
+elif [ "$version" = "Linux Mint 18" || "$version" = "Ubuntu 16" ]
 then
-sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-deb http://repo.pritunl.com/stable/apt xenial main
-EOF
+	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+	deb http://repo.pritunl.com/stable/apt xenial main
+	EOF
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get update
-sudo apt-get install pritunl-client-electron
-#pritunl-client-electron
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	sudo apt-get update
+	sudo apt-get install pritunl-client-electron
 
-elif [ "$version" = "Ubuntu 18" || "$version" = "Mint 18" ]
+elif [ "$version" = "Ubuntu 19" ]
 then
-sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
-deb http://repo.pritunl.com/stable/apt disco main
-EOF
+	sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+	deb http://repo.pritunl.com/stable/apt disco main
+	EOF
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-sudo apt-get update
-sudo apt-get install pritunl-client-electron
-#pritunl-client-electron
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+	sudo apt-get update
+	sudo apt-get install pritunl-client-electron
 else 
-    echo "Not OK"
+    	echo "$version not supported by this script, only Ubuntu 16, Ubuntu 18, Linux Mint 18 and Linux Mint 19 are supported!!"
 fi
+exit
 #################
 ####################
 #fi
 ####################
 #lsb_release -a | grep -i disco
+#### Create desktop icon and launcher to desktop
 tee /home/$USER/Desktop/Pritunl.desktop << EOF
 [Desktop Entry]
 Name=Pritunl Client
@@ -58,3 +56,5 @@ Categories=Network;TrayIcon;
 StartupWMClass=pritunl
 EOF
 chmod +x /home/$USER/Desktop/Pritunl.desktop
+## Start the Pritunl client
+pritunl-client-electron
