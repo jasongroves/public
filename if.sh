26af0788952d73fc3a530960d78c805ac2eff40c
Jason <jason@openbyte.co.za>
2020-01-24 14:34:40 +0200

    New commit

---
+++ if.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+ 
+#version=$(cat /etc/issue | cut -d "." -f1)
+#version=`cat /etc/issue | cut -d "." -f1`
+ 
+if [ "$version" = "Linux Mint 19" ]
+then
+    echo "Version is Linux Mint 19"
+
+ 
+elif [ "$version" = "Ubuntu 19" ]
+then
+    echo "Version is Ubuntu Mint 19"
+else
+    echo "Not OK"
+fi
+#################
+#cat /etc/issue | grep -i -E "Ubuntu 19|Mint 19"
+#if [ $? = 0 ]
+#then
+#sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+#deb http://repo.pritunl.com/stable/apt bionic main
+#EOF
+#
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+#sudo apt-get update
+#sudo apt-get install pritunl-client-electron -y
+#pritunl-client-electron
+#fi
+####################
+#cat /etc/issue | grep -i -E "Ubuntu 18|Mint 18"
+#if [ $? = 0 ]
+#then
+#sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+#deb http://repo.pritunl.com/stable/apt xenial main
+#EOF
+#
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+#sudo apt-get update
+#sudo apt-get install pritunl-client-electron
+#pritunl-client-electron
+#fi
+####################
+#lsb_release -a | grep -i disco
+#if [ $? = 0 ]
+#then
+#sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+#deb http://repo.pritunl.com/stable/apt disco main
+#EOF
+#
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+#sudo apt-get update
+#sudo apt-get install pritunl-client-electron
+#pritunl-client-electron
+#fi
+#tee /home/$USER/Desktop/Pritunl.desktop << EOF
+#[Desktop Entry]
+#Name=Pritunl Client
+#Comment=Pritunl openvpn client
+#Exec=/usr/lib/pritunl_client_electron/Pritunl
+#Terminal=false
+#Type=Application
+#StartupNotify=false
+#Icon=pritunl_client_electron
+#Categories=Network;TrayIcon;
+#StartupWMClass=pritunl
+#EOF
+#chmod +x /home/$USER/Desktop/Pritunl.desktop
+#
